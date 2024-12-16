d2e9d3f050e850a8fe0ff824d36a5ace24891578
Peter Moelzer <moelzerpeter@gmail.com>
2024-12-16 11:07:26 +0100

    bug fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0ZyitxBDK9JcvgTXFOQb+Aq2uHHolBvhoiC+dMHONps=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3wIpB/GLHZc3iroaLhcrbgN8VRwKiwfuO95ccYo5VwM=\"\n}"
